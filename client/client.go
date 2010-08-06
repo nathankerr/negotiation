383b3e66da5b9a8f56485ece9c1ce353273a43fb
Nathan Kerr <nathan@nathankerr.com>
2010-08-06 13:18:38 +0200

    Client runs more than one value

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"rpc"
 )
 
@@ -14,6 +15,10 @@ type Quotient struct {
 	Quo, Rem int
 }
 
+func Dial(addr string, requirements string) (*rpc.Client, os.Error) {
+	return rpc.DialHTTP("tcp", addr)
+}
+
 func main() {
 	client, err := rpc.DialHTTP("tcp", ":1234")
 	if err != nil {
@@ -23,9 +28,12 @@ func main() {
 	// Synchronous call
 	args := &Args{7,8}
 	var reply int
-	err = client.Call("Arith.Multiply", args, &reply)
-	if err != nil {
-		log.Exit("arith error:", err)
+	for i := 0; i < 10; i++ {
+		args.A = i
+		err = client.Call("Arith.Multiply", args, &reply)
+		if err != nil {
+			log.Exit("arith error:", err)
+		}
+		fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
 	}
-	fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
 }
